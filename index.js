0c4d23d8fd12d0adb8ab9efdeac2907af023709c
Juvet <juvettaj@gmail.com>
2022-03-16 17:35:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.use("/login", (req, res) => {
+  res.send({
+    token: "todo123",
+  });
+});
+
+app.listen(8000, () => console.log("API IS RUNNING ON PORT 8000"));
